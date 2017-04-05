eb0b8272494f4e7bb1239019395f364d8c508ec2
Jean Manas <jean.manas@outsourced.ph>
2017-04-05 18:19:16 +0800

    Initial Commit

---
+++ mtrckr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mtrckr
+//
+//  Created by User on 4/5/17.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
