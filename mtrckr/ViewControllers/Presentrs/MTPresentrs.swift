8ae83a8d019cc765ca9f00107451ca14322a430c
Jean Manas <jean.manas@outsourced.ph>
2017-06-02 10:17:49 +0800

    setup Presentr to use in authentication view controllers animation

---
+++ mtrckr/ViewControllers/Presentrs/MTPresentrs.swift
@@ -0,0 +1,17 @@
+//
+//  AuthentionPresentr.swift
+//  mtrckr
+//
+//  Created by User on 6/2/17.
+//
+//
+
+import UIKit
+import Presentr
+
+class AuthenticationPresentr {
+    
+    init() {
+        
+    }
+}
---
+++ mtrckr/Views/Class/Base Classes/MTTextField.swift
@@ -0,0 +1,21 @@
+//
+//  MTTextField.swift
+//  mtrckr
+//
+//  Created by User on 6/2/17.
+//
+//
+
+import UIKit
+
+class MTTextField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
